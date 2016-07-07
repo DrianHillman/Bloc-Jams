ac01edec05f4e446ae4ac09f102e787dd09c3103
Drian Hillman <dhillman24@gmail.com>
2016-07-07 00:45:32 +0000

    Add play from player bar feature | Merge of Assignment33

--- scripts/album.js
+++ scripts/album.js
@@ -189,6 +189,19 @@ var setVolume = function(volume){
   }
 };
 
+var togglePlayFromPlayerBar = function(){
+  
+  if (currentSoundFile.isPaused()) {
+    $(getSongNumberCell(currentlyPlayingSongNumber)).html(pauseButtonTemplate);
+    $playPauseButton.html(playerBarPauseButton);
+    currentSoundFile.play();
+  } else {
+    $(getSongNumberCell(currentlyPlayingSongNumber)).html(playButtonTemplate);
+    $playPauseButton.html(playerBarPlayButton);
+    currentSoundFile.pause();
+  }
+};
+
 var getSongNumberCell= function(number){
   
   return $('.song-item-number[data-song-number="' + number + '"]');
@@ -271,10 +284,12 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
 $(document).ready(function(){
   setCurrentAlbum(albumPicasso);
   setupSeekBars();
   $previousButton.click(previousSong);
   $nextButton.click(nextSong);
+  $playPauseButton.click(togglePlayFromPlayerBar);
 });
